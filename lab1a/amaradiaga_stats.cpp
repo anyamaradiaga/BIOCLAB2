38727d68e1a335a797cf48679f04f66da941f55b
anyamaradiaga <anyamaradiaga91@gmail.com>
2022-02-08 11:35:53 -0600

    unsaved changes for functions

--- lab1a/amaradiaga_stats.cpp
+++ lab1a/amaradiaga_stats.cpp
@@ -5,57 +5,126 @@ Description:
 */
 #include "amaradiaga_stats.hpp"
 
+//namespace class
+//then constructor
 //calculating the maximum
-double calcMax(double new_array[], int numrows)
+
+using namespace std;
+
+class Max{
+	public:
+double calcMax(double array[])
 {
 	int i;
-	double max = new_array[0];
-	for (i = 0; i < numrows; i++) //going through every row
+	double max = array[0];
+	for (i = 0; i < sizeof(array); i++) 
 	{
-		if (new_array[i] > max)
+		if (array[i] > max)
 		{
-			max = new_array[i];
+			max = array[i];
 		}
 	}
 	return(max);
 }
+};
+
 
 //calculating the minimum
-double calcMin(double new_array[], int numrows)
+class Min{
+	public:
+double calcMin(double array[])
 {
 	int i;
-	double min = new_array[0];
-	for (i = 0; i < numrows; i++) //going through every row
+	double min = array[0];
+	for (i = 0; i < sizeof(array); i++) //going through every row
 	{
-		if (new_array[i] < min)
+		if (array[i] < min)
 		{
-			min = new_array[i];
+			min = array[i];
 		}
 	}
 	return(min);
 }
+};
 
+class Mean{
+	public:
 //calculate the mean 
-float calcMean(double new_array[], int numrows)
+float calcMean(double array[])
 {
 	double mean = 0;
 	int i;
-	for (i = 0; i < numrows; i++) //going through every row
+	for (i = 0; i < sizeof(array); i++) //going through every row
 	{
-		mean += new_array[i];
+		mean += array[i];
 	}
-	mean = mean / numrows;
+	mean = mean / sizeof(array);
 	return(mean);
 }
+};
 
+class Std{
+	public:
 //calculating the standard deviation
-double calcSd(double new_array[], int numrows) {
+double calcSd(double array[]) {
 	int i;
 	float result = 0.0;
-	float m = calcMean(new_array, numrows);
-	for (i = 0; i < numrows; i++)
+	Mean calc; //create instance of mean class
+	float m = calc.calcMean(array);
+	for (i = 0; i < sizeof(array); i++)
 	{
-		result += pow((new_array[i] - m), 2);
+		result += pow((array[i] - m), 2);
 	}
-	return sqrt(result / numrows); //return the standard deviation
+	return sqrt(result / sizeof(array)); //return the standard deviation
 }
+};
+
+class Histogram{
+	public:
+	double calcHist(double array[]){ //input array
+		//bin center should be at the mean
+		Mean calc; //create instance of mean class
+		double bin_center[] = {calc.calcMean(array)};
+
+		//find max of data
+		Max calc_max;
+		double max = calc_max.calcMax(array);
+
+		//find min of data
+		Min calc_min; 
+		double min = calc_min.calcMin(array); 
+
+		//get standard deviation
+		Std calc_std; 
+		double std = calc_std.calcSd(array);
+
+		//each bin should have a width of 0.4*sample standard deviation
+		double bin_width = 0.4 * std; 
+
+		//bins should begin and end at sample mean ±3*(sample stddev), respectively.
+		double bin_start = 0.3*std, bin_end =(-0.3)*std; 
+
+		//vector<int>binedge;
+		int binedge[]={}; //array for binedge
+		binedge[0]=bin_start; //set the first edge to the bin start
+		for (int i = bin_start; i < bin_end; i++) {
+		binedge[i] = binedge[i - 1] + bin_width;
+		}
+
+		for (int i = 1; i <= bin_end; i++) {
+		bin_center[i] = binedge[i] + ((1.0 / 2.0) * bin_width);
+		}
+
+		double count_array[]={}; //count array for histogram
+		double count=0; 
+		for(int i=0; i<= sizeof(array);i++){
+			for( int j=bin_start; j<=bin_end; j++){
+				if(array[i] < binedge[j + 1] && binedge[j] <= array[i]){
+					count ++;
+				}
+			}
+			count_array[i] = count; //array that stores the count total
+		}
+		return(count);
+	}
+};
--- lab1a/amaradiaga_stats.hpp
+++ lab1a/amaradiaga_stats.hpp
@@ -8,17 +8,26 @@ Description:
 
 //define libraries
 #include <stdio.h>
+#include <cstdlib>
+#include <cstdio> 
 #include <stdlib.h>
-#include<math.h>
+#include <iostream>  
+#include <math.h>
+#include <vector>
 #include <array>
+using namespace std; //used for cout, min, max arrays...
+//using namespace myclass max; and define in cpp
 
-//max function declaration. 
-double calcMax(double new_array[], int numrows);
-//min function declaration.
-double calcMin(double new_array[], int numrows);
-//mean function declaration. 
-float calcMean(double new_array[], int numrows);
-//standard deviation function declaration.
-double calcSd(double new_array[], int numrows);
+//max function definition. 
+double calcMax(double array[]);
+//min function definition.
+double calcMin(double array[]);
+//mean function definition. 
+float calcMean(double array[]);
+//standard deviation function definition.
+double calcSd(double array[]);
+//histogram function definition
+double calcHist(double array[]);
+
 
 #endif 
